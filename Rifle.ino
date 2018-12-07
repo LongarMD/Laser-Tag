bde351653bb2cdae509cf316f8866be19206d660
LongarMD <mark.longar@gmail.com>
2018-12-07 17:48:52 +0100

    Add files via upload

--- Rifle.ino
+++ Rifle.ino
@@ -24,6 +24,14 @@ uint8_t codeProtocol;  // The type of code
 uint32_t codeValue;    // The data bits if type is not raw
 uint8_t codeBits;      // The length of the code in bits
 
+//DEFINICIJA IR KOD
+#define HIT_CODE 281600286
+#define MAX_LIVES 4
+
+int ledsGPIO[] = { 10 , 11 , 12, 13};
+bool ledsStatus[] = { 1, 1, 1, 1};
+volatile int usedLives = 0;
+
 //These flags keep track of whether we received the first code 
 //and if we have have received a new different code from a previous one.
 bool gotOne, gotNew; 
@@ -32,33 +40,62 @@ void setup() {
   //Receiver
   gotOne=false; gotNew=false;
   codeProtocol=UNKNOWN; 
-  codeValue=0; 
+  codeValue=0;
+
+  for (int i = 0; i < MAX_LIVES; i++ ) {
+    pinMode(ledsGPIO[i], OUTPUT);
+  }
 
   //Sender
   pinMode(9, INPUT);
   buttonPressed = false;
+
+  //Interrupt
+  attachInterrupt(digitalPinToInterrupt(2), ISR_ReceiveSignal, CHANGE);
   
   Serial.begin(9600);
   Serial.println(F("Type any character and press enter. We will send the recorded code."));
   
   myReceiver.enableIRIn(); // Start the receiver
+  RefreshLEDS();
 }
+
+void loop() {
+
+  if(usedLives >= MAX_LIVES){
+    GameOver();
+  }
+  
+  // Shooting
+  if (digitalRead(9) == HIGH && buttonPressed == false) {
+    mySender.send(NEC,0x10c8e11e, 0);//Sony DVD power A8BCA, 20 bits
+    //mySender.send(NEC,0x61a0f00f);
+    
+    buttonPressed = true;
+    Serial.println(F("Sent signal."));
+  }
+  else if (digitalRead(9) == LOW && buttonPressed == true) { buttonPressed = false; }
+}
+
 
 // Stores the code for later playback
 void storeCode(void) {
   gotNew=true;    gotOne=true;
   codeProtocol = myDecoder.protocolNum;
-  Serial.print(F("Received "));
-  Serial.print(Pnames(codeProtocol));
+
+  // Ker uporabljamo interrupte, serial printov ni pametno uporabljati
+  //Serial.print(F("Received "));
+  //Serial.print(Pnames(codeProtocol));
+  
   if (codeProtocol==UNKNOWN) {
-    Serial.println(F(" saving raw data."));
+    //Serial.println(F(" saving raw data."));
     myDecoder.dumpResults();
     codeValue = myDecoder.value;
   }
   else {
     if (myDecoder.value == REPEAT_CODE) {
       // Don't record a NEC repeat value as that's useless.
-      Serial.println(F("repeat; ignoring."));
+      //Serial.println(F("repeat; ignoring."));
     } else {
       codeValue = myDecoder.value;
       codeBits = myDecoder.bits;
@@ -68,19 +105,43 @@ void storeCode(void) {
   }
 }
 
-void loop() {
+void GameOver() {
+  for (int i = 0; i < MAX_LIVES; i++ ) {
+    digitalWrite(ledsGPIO[i], LOW);
+  }
+  delay(500);
+  
+  for (int i = 0; i < MAX_LIVES; i++ ) {
+    digitalWrite(ledsGPIO[i], HIGH);
+  }
+  delay(500);
+}
+
+void CheckHit(){
+  //TODO: Koda za "respawn"
+  
+   if (codeValue == HIT_CODE && usedLives < MAX_LIVES) {
+    ledsStatus[usedLives] = 0;
+    usedLives++;
+  }
+  RefreshLEDS();
+}
+
+void RefreshLEDS(){
+  //Refresh LEDs
+  for (int i = 0; i < MAX_LIVES; i++ ) {
+    digitalWrite(ledsGPIO[i], ledsStatus[i]);
+  }  
+}
+
+void ISR_ReceiveSignal()
+{
   if (myReceiver.getResults()) {
     myDecoder.decode();
     storeCode();
+    
+    CheckHit();
+    
     myReceiver.enableIRIn(); // Re-enable receiver
   }
-
-  if ((Serial.read() != -1 || digitalRead(9) == HIGH) && buttonPressed == false) {
-    mySender.send(SONY,0x10c8e11e, 20);//Sony DVD power A8BCA, 20 bits
-    //mySender.send(NEC,0x61a0f00f,0);
-
-    buttonPressed = true;
-    Serial.println(F("Sent signal."));
-  }
-  else if (digitalRead(9) == LOW && buttonPressed == true) { buttonPressed = false; }
 }
