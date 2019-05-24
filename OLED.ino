f857dd90a4851da583cdd5ec0f207b91426dd7fa
LongarMD <mark.longar@gmail.com>
2019-05-24 16:59:48 +0200

    OLED

--- OLED.ino
+++ OLED.ino
@@ -1,32 +1,53 @@
 #include <Adafruit_SSD1306.h>
 
-#define OLED_CLK 6
-#define OLED_MOSI 7
-#define OLED_RESET 8
-#define OLED_DC 9
-#define OLED_CS 10
+#define OLED_CLK 9
+#define OLED_MOSI 10
+#define OLED_RESET 11
+#define OLED_DC 12
+#define OLED_CS 13
 
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+#define SSD1306_128_64
+// #define SSD1306_128_32
+// #define SSD1306_96_16
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00001100, B00110000,
+  B00011110, B01111000,
+  B00111111, B11111100,
+  B00111111, B11111100,
+  B01111111, B11111110,
+  B01111111, B11111110,
+  B01111111, B11111110,
+  B00111111, B11111100,
+  B00111111, B11111100,
+  B00011111, B11111000,
+  B00001111, B11110000,
+  B00000111, B11100000,
+  B00000011, B11000000,
+  B00000001, B10000000
+};
+
+Adafruit_SSD1306 display(128,64,OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC);
-  //rtc.setDOW(WEDNESDAY);     // Set Day-of-Week to SUNDAY
-  //rtc.setTime(19, 02, 0);     // Set the time to 12:00:00 (24hr format)
-  //rtc.setDate(7, 12, 2017);   // Set the date to June 6th, 2017
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(3);
-  display.setCursor(0,0);
+  display.setTextColor(BLACK);
+  display.setTextSize(4);
+  display.setCursor(10,0);
   display.display();
 }
 
 void loop() {
   display.clearDisplay();
-
-  display.setCursor(0,0);
-  display.print("OwO");
-
-  display.display(); 
+  testdrawbitmap(20,20,14,16,1);
   delay(1000);
+}
 
+
+void testdrawbitmap(int WIDTH, int HEIGHT, int bmpHeight, int bmpWidth, int flag) {
+  display.drawBitmap(
+    WIDTH,
+    HEIGHT,
+    logo_bmp, bmpWidth, bmpHeight, flag);
+  display.display();
+  delay(1000);
 }
